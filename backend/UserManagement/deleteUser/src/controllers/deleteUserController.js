592d7ad3da3a0c93b54dd7627aad352c62ab1136
JessJoha <jessicajcenteno@gmail.com>
2025-01-26 13:22:43 -0500

    remove unnecessary comments for cleaner code

--- backend/UserManagement/deleteUser/src/controllers/deleteUserController.js
+++ backend/UserManagement/deleteUser/src/controllers/deleteUserController.js
@@ -1,17 +1,17 @@
 const { User } = require('../models/userModel');
 
 const deleteUserController = async (req, res) => {
-  const { userId } = req.params; // Suponiendo que se envía el ID del usuario a eliminar como parámetro de ruta
+  const { userId } = req.params; 
 
   try {
-    // Busca al usuario por ID
+    
     const user = await User.findOne({ where: { userId } });
 
     if (!user) {
       return res.status(404).json({ message: 'Usuario no encontrado' });
     }
 
-    // Elimina al usuario
+   
     await user.destroy();
 
     return res.status(200).json({ message: 'Usuario eliminado exitosamente' });
--- backend/UserManagement/deleteUser/src/server.js
+++ backend/UserManagement/deleteUser/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./config/db'); 
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes'); // Importa las rutas de usuarios
+const userRoutes = require('./routes/userRoutes');
 
 
 dotenv.config();
@@ -12,22 +12,22 @@ const app = express();
 
 app.use(express.json());
 
-// Middleware para manejar errores
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('¡Ocurrió un error!');
 });
 
-// Middleware para habilitar CORS
+
 const corsOptions = {
   origin: process.env.CORS_ALLOWED_ORIGIN || '*',
 };
 app.use(cors(corsOptions));
 
-// Uso de rutas
-app.use('/user', userRoutes); // Ajusté la base de la ruta a `/user`
 
-// Sincronización con la base de datos
+app.use('/user', userRoutes); 
+
+
 sequelize.sync()
   .then(() => {
     console.log('La base de datos y las tablas fueron creadas con éxito');
@@ -36,7 +36,7 @@ sequelize.sync()
     console.error('Error al sincronizar la base de datos:', err);
   });
 
-// Inicialización del servidor
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
