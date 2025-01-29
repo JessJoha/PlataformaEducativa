175d12592373957a0dc64f1456e910c0bb8d2c6c
JessJoha <jessicajcenteno@gmail.com>
2025-01-29 15:25:33 -0500

    feat: add Course deletion controller

---
+++ backend/CoursesManagment/deleteCourse/src/controller/deleteCourseController.js
@@ -0,0 +1,28 @@
+
+const { Course } = require('../createCourses/src/model/courseModel');
+
+
+const deleteCourse = async (req, res) => {
+  const { courseId } = req.params;
+
+  try {
+    
+    const course = await Course.findOne({ where: { courseId } });
+
+    if (!course) {
+      return res.status(404).json({ error: 'Course not found' });
+    }
+
+   
+    await course.destroy();
+
+    return res.status(200).json({ message: 'Course deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting course:', error);
+    return res.status(500).json({ error: 'Error deleting course' });
+  }
+};
+
+module.exports = {
+  deleteCourse,
+};
