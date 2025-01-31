080b6e7d13a20757dc057d0f6a9352af3b8ff5eb
Johan Llanga <andijohan9393@hotmail.com>
2025-01-30 23:24:33 -0500

    feat(createEvaluation): Add new folder for the Evaluations microservice

---
+++ backend/EvaluationsManagement/createEvaluation/src/Controller/evaluationController.js
@@ -0,0 +1,30 @@
+
+const { Course } = require('../Model/evaluationModel');
+
+const createCourse = async (req, res) => {
+  const { nameCourse, description, duration } = req.body;
+
+  try {
+    if (!nameCourse || !description || !duration) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    const newCourse = await Course.create({
+      nameCourse,
+      description,
+      duration,
+    });
+
+    return res.status(201).json({   
+      message: 'Course created successfully',
+      course: newCourse,
+    });
+  } catch (error) {
+    console.error('Error creating course:', error);
+    return res.status(500).json({ error: 'Error creating course' });
+  }
+};
+
+module.exports = {
+  createCourse,
+};
