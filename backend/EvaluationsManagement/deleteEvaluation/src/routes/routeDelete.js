1c07ac708c217172a4155972a9644d47c3554524
Johan Llanga <andijohan9393@hotmail.com>
2025-02-01 02:15:04 -0500

    feat(deleteEvaluation): Add route for evaluation deletion

---
+++ backend/EvaluationsManagement/deleteEvaluation/src/routes/routeDelete.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { deleteEvaluation } = require('../controller/deleteEvaluationController');
+
+const router = express.Router();
+
+router.delete('/delete/:evaluationId', deleteEvaluation);
+
+module.exports = router;
