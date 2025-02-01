f355c113fb08d97d259ca32c9fac594a314817dc
Johan Llanga <andijohan9393@hotmail.com>
2025-02-01 16:27:38 -0500

    feat(createEvaluation): Updating the .env to create assessments

--- backend/EvaluationsManagement/createEvaluation/src/config/db.js
+++ backend/EvaluationsManagement/createEvaluation/src/config/db.js
@@ -8,7 +8,9 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,   
   {
     host: process.env.DB_HOST, 
-    dialect: 'postgresql',         
+    dialect: 'postgres',  
+    port: process.env.DB_PORT || 5432,  
+    logging: false 
   }
 );
 
