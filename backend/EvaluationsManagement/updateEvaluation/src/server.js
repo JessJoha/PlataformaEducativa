231ac9d9b69ad160b7d6ef418d0ab3a8923559f9
Johan Llanga <andijohan9393@hotmail.com>
2025-02-02 21:34:53 -0500

    📦️ feat(listEvaluation): Adding server.js code

---
+++ backend/EvaluationsManagement/updateEvaluation/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const evaluationRoutes = require('./routes/routeUpdate');
+const sequelize = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/evaluations', evaluationRoutes);
+
+const PORT = process.env.PORT || 4009;
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected');
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.error('Database connection failed:', error);
+  }
+});
