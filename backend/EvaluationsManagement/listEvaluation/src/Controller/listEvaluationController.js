83d81b226dfcc0f949cdd2f816a792f8d753cfba
Johan Llanga <andijohan9393@hotmail.com>
2025-02-02 20:37:54 -0500

    📦️ feat(listEvaluation): Adding listEvaluationController.js code

---
+++ backend/EvaluationsManagement/listEvaluation/src/Controller/listEvaluationController.js
@@ -0,0 +1,13 @@
+const { Evaluation } = require('../../../createEvaluation/src/Model/evaluationModel');
+
+const listEvaluations = async (req, res) => {
+  try {
+    const evaluations = await Evaluation.findAll();
+    return res.status(200).json(evaluations);
+  } catch (error) {
+    console.error('Error listing evaluations:', error);
+    return res.status(500).json({ error: 'Error listing evaluations' });
+  }
+};
+
+module.exports = { listEvaluations };
