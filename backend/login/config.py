6d9e7a180792fdde269d7a47227eb68178070459
JessJoha <jessicajcenteno@gmail.com>
2025-02-10 15:13:20 -0500

    🐛  feat(test): config.py file for login was updated

--- backend/login/config.py
+++ backend/login/config.py
@@ -7,9 +7,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_ALLOWED_ORIGIN = os.environ.get('CORS_ALLOWED_ORIGIN')
-    ACCESS_TOKEN_EXPIRATION = int(os.environ.get('ACCESS_TOKEN_EXPIRATION'))
+    ACCESS_TOKEN_EXPIRATION = int(os.environ.get('ACCESS_TOKEN_EXPIRATION', 3600))
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'info')
     LOGIN_SERVICE_PORT = int(os.environ.get('LOGIN_SERVICE_PORT', 5000))
 
 
-    CREATE_USER_SERVICE_URL = os.environ.get('CREATE_USER_SERVICE_URL', 'http://localhost:3000')
+    CREATE_USER_SERVICE_URL = os.environ.get('CREATE_USER_SERVICE_URL', 'http://13.216.132.78:3000/users/register')
