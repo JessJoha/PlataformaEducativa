9ccec925e3999e3001243733b3616bfa7a87ecdb
Johan Llanga <andijohan9393@hotmail.com>
2025-02-09 22:32:16 -0500

    🐛 🔥 feat(test): Update the dockerfile list file

---
+++ backend/EvaluationsManagement/listEvaluation/src/Model/listEvaluationModel.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Evaluation = sequelize.define('Evaluation', {
+  evaluationId: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  nameEvaluation: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  questions: {
+    type: DataTypes.JSON,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+});
+
+module.exports = { Evaluation };
