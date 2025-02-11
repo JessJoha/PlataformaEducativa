4eae4292926317e04919449501737ea48d6c036c
JessJoha <jessicajcenteno@gmail.com>
2025-02-10 23:31:40 -0500

    🚨 🐛 feat(test): yml file for login was updated

--- backend/login/config.py
+++ backend/login/config.py
@@ -3,13 +3,13 @@ from jwt_config import JWT_SECRET
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    JWT_SECRET_KEY = JWT_SECRET
-    SQLALCHEMY_DATABASE_URI = f"mysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL')  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_ALLOWED_ORIGIN = os.environ.get('CORS_ALLOWED_ORIGIN')
     ACCESS_TOKEN_EXPIRATION = int(os.environ.get('ACCESS_TOKEN_EXPIRATION', 3600))
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'info')
     LOGIN_SERVICE_PORT = int(os.environ.get('LOGIN_SERVICE_PORT', 5000))
 
-
     CREATE_USER_SERVICE_URL = os.environ.get('CREATE_USER_SERVICE_URL', 'http://13.216.132.78:3000')
+
