86f2310d87402dee412b0da20578b91a998f9f3e
JessJoha <jessicajcenteno@gmail.com>
2025-02-11 13:13:00 -0500

    🐛 🚨 feat(test): config file for login was updated

--- backend/login/config.py
+++ backend/login/config.py
@@ -3,12 +3,12 @@ from jwt_config import JWT_SECRET
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET') 
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
+    CREATE_USER_SERVICE_URL = os.environ.get('CREATE_USER_SERVICE_URL', 'http://13.216.132.78:3000/users/register')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL')  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_ALLOWED_ORIGIN = os.environ.get('CORS_ALLOWED_ORIGIN')
     ACCESS_TOKEN_EXPIRATION = int(os.environ.get('ACCESS_TOKEN_EXPIRATION', 3600))
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'info')
     LOGIN_SERVICE_PORT = int(os.environ.get('LOGIN_SERVICE_PORT', 5000))
-    CREATE_USER_SERVICE_URL = os.environ.get('CREATE_USER_SERVICE_URL', 'http://13.216.132.78:3000')
 
