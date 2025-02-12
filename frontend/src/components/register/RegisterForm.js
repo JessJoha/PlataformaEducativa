de88b1c5b1d74152cf3b24f29b226f952f778840
JessJoha <jessicajcenteno@gmail.com>
2025-02-11 21:05:22 -0500

    🚨 feat(test): yml file for login was updated

--- frontend/src/components/register/RegisterForm.js
+++ frontend/src/components/register/RegisterForm.js
@@ -1,42 +1,51 @@
 import React, { useState } from 'react';
-  const RegisterForm = () => {
+import { useNavigate } from 'react-router-dom'; // Reemplazamos useHistory con useNavigate
+
+const RegisterForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState(''); // Estado para el mensaje de éxito
+  const navigate = useNavigate(); // Usamos useNavigate en lugar de useHistory
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-   
     if (!username || !password) {
       setErrorMessage('El nombre de usuario y la contraseña son requeridos');
       return;
     }
 
     try {
-      
       const response = await fetch("http://13.216.132.78:3000/users/register", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, password }) 
+        body: JSON.stringify({ username, password })
       });
 
       const data = await response.json();
 
       if (response.ok) {
-        alert('Usuario registrado exitosamente');
+        setSuccessMessage('Usuario registrado exitosamente');
         setUsername('');
         setPassword('');
         setErrorMessage('');
       } else {
         setErrorMessage(data.error || 'Error al registrar el usuario');
+        setSuccessMessage('');
       }
     } catch (error) {
       console.error('Error al registrar el usuario:', error);
       setErrorMessage('Hubo un error al procesar tu solicitud');
+      setSuccessMessage('');
     }
+  };
+
+  // Función para regresar
+  const handleBack = () => {
+    navigate('/');  // Redirige a la página principal o a la página que desees
   };
 
   return (
@@ -66,8 +75,15 @@ import React, { useState } from 'react';
         <div className="form-group">
           <button type="submit">Registrar</button>
         </div>
+
         {errorMessage && <div id="error-message" className="error-message">{errorMessage}</div>}
+        {successMessage && <div id="success-message" className="success-message">{successMessage}</div>}
       </form>
+
+      {/* Botón de regresar */}
+      <button onClick={handleBack} style={{ marginTop: '20px' }}>
+        Regresar
+      </button>
     </div>
   );
 };
